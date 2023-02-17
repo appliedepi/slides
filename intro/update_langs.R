42a8539e050f40d55576d6ef906114e31a4ce459
Neale Batra <52152191+nsbatra@users.noreply.github.com>
2023-02-17 14:23:01 +0100

    mod 2 ua and es

--- intro/update_langs.R
+++ intro/update_langs.R
@@ -6,7 +6,7 @@ pacman::p_load(rio, googlesheets4, tidyr, stringr, janitor)
 # list all langs desired
 langs <- c("ua", "es")
 slides_folders <- list.files(here::here("intro", "en"), recursive = FALSE)
-specific_folders <- c("module_6-1")
+specific_folders <- c("module_2")
 
 # Create function
 update_slides <- function(folder = "module_10", rmd = "module_10.Rmd", lang_to = "fr"){
