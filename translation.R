d800214c7773efd7749547da84f1e27621a570d0
Luong Nguyen Thanh <ph.ntluong95@gmail.com>
2024-03-29 01:11:35 +0100

    archived old slides, translate to spanish

---
+++ translation.R
@@ -0,0 +1,81 @@
+# NEW LANGUAGE TRANSLATION PROTOCOL ------------------------------------------------------------------------
+
+# 0. Create a new branch for a new language
+
+# 1. First install the babeldown package
+#install.packages('babeldown', repos = c('https://ropensci.r-universe.dev', 'https://cloud.r-project.org'))
+
+
+# 2.Setup the environment variable
+Sys.setenv("DEEPL_API_URL" = "https://api.deepl.com")
+Sys.setenv(DEEPL_API_KEY = "287d5481-9d96-8500-228c-6f98cfb3c576")
+
+
+
+# 3. Indicate modules to be translated
+module_list = c("intro1",
+                "intro2", "intro3", "intro4", "intro5_1", "intro5_2",
+                "intro6", "intro7_1", "intro7_2", "intro9",
+                "intro10")
+
+base_directory <- here::here("intro", "2024 update")
+
+languages <- c("es") # "fr", "pt" Add more languages as needed
+
+# 4. Write a loop to run the translation for each module
+for (i in 1:length(module_list)) {
+  for (j in 1:length(languages)) {
+    babeldown::deepl_translate_quarto(
+      book_path = paste0(base_directory, "/", module_list[i]),
+      chapter = paste0(module_list[i], ".Rmd"),
+      force = TRUE,
+      render = FALSE, # Whether to run babelquarto::render_bool() after translation.
+      source_lang = "EN",
+      target_lang = toupper(languages[j]),
+      formality = "less")
+  }
+}
+
+
+# 5. Create new folder for language versions and move files
+
+
+# Function to process each module
+create_LanguageModule <- function(base_directory, module_name, languages) {
+  for (lang in languages) {
+    # Construct the file and folder names
+    file_name <- paste0(module_name, ".", lang, ".Rmd")
+    folder_name <- paste0(module_name, ".", lang)
+    file_path <- file.path(base_directory, module_name, file_name)
+    new_folder_path <- file.path(base_directory, folder_name)
+    css_file_path <- file.path(base_directory, module_name, "xaringan-themer.css") # Path to the CSS file
+
+    # Check if the file exists
+    if (file.exists(file_path)) {
+      # Create language-specific folder if it doesn't exist
+      if (!dir.exists(new_folder_path)) {
+        dir.create(new_folder_path, recursive = TRUE)
+      }
+
+      # Move the .Rmd files
+      file.rename(from = file_path, to = file.path(new_folder_path, file_name))
+
+      # Check and copy the xaringan-themer.css file if it exists
+      if (file.exists(css_file_path)) {
+        file.copy(from = css_file_path, to = new_folder_path, recursive = TRUE)
+      }
+    }
+  }
+}
+
+# Apply the function to each module
+for (module in module_list) {
+  create_LanguageModule(base_directory, module, languages)
+}
+
+# 6. Review the translation
+
+
+
+
+
